68c4752574a6114765ea2b72b8f3bc0b3422b631
Neh Reh <ne.n.reh@baypath.net>
2024-05-30 14:44:13 +0000

    containers n stuff ; fixin overflows

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.hosts << "ponchobanana-pulseceramic-3000.codio.io"
+  config.hosts << "connectalarm-spellrocket-3000.codio.io"
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
